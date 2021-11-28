4a9006c9424b5f35ae3d3fcd81d5775f27de6119
kanishka_bandara <wmkubandara@gmail.com>
2021-11-28 11:03:18 +0530

    ADD::Folder generate method

--- src/main/java/com/aradnab/code_gen/controllers/GramaCodeGenerateController.java
+++ src/main/java/com/aradnab/code_gen/controllers/GramaCodeGenerateController.java
@@ -4,10 +4,8 @@
  */
 package com.aradnab.code_gen.controllers;
 
-import com.aradnab.code_gen.models.Column;
 import com.aradnab.code_gen.models.Database;
-import com.aradnab.code_gen.models.Table;
-import java.util.List;
+import java.io.File;
 
 /**
  *
@@ -17,16 +15,51 @@ public class GramaCodeGenerateController {
   public static GramaCodeGenerateController defaultController=  new GramaCodeGenerateController();
   
   public void generate() throws Exception{
+      String projectName = "grama_1";
+      String folderPath = "/opt/lampp/htdocs";
+      String mainFolderPath = folderPath+"/"+projectName;
+      String formsHtmlFolderPath = mainFolderPath+"/Views/forms";
+      File mainFolder = new File(mainFolderPath);
+      File formFolder = new File(formsHtmlFolderPath);
+      
       Database database = DBController.defaultController.getDatabaseDetails();
-      System.out.println("##### [Database = "+database.getSqlName()+"] #####");
-      List<Table> tables = database.getTables();
-      for (Table table : tables) {
-          System.out.println("--[Table = "+table.getInitName()+"]--");
-          List<Column> columns = table.getColumns();
-          for (Column column : columns) {
-              System.out.println(column.toString());
+      
+      //Creating project folder
+      createFolder(mainFolder,"Main");
+      
+      
+//      System.out.println("##### [Database = "+database.getSqlName()+"] #####");
+//      List<Table> tables = database.getTables();
+//      for (Table table : tables) {
+//          System.out.println("--[Table = "+table.getInitName()+"]--");
+//          List<Column> columns = table.getColumns();
+//          for (Column column : columns) {
+//              System.out.println(column.toString());
+//          }
+//          System.out.println("------");
+//      }
+  }
+  
+  public void createFolder(File folder,String name){
+    if (!folder.exists()) {
+          if (folder.mkdir()) {
+              System.out.println(name+" "+"Folder has been created.");
+          }else{
+              System.out.println(name+" "+"Folder has not been created.");
           }
-          System.out.println("------");
+      }else{
+          System.out.println(name+" "+"Folder is existing.About to delete.");
+          if (folder.delete()) {
+                System.out.println(name+" "+"Folder has been deleted.");
+                if (folder.mkdir()) {
+                    System.out.println(name+" "+"Folder has been newly created.");
+                }else{
+                    System.out.println(name+" "+"Folder has not been created.");
+                }
+          }else{
+              System.out.println(name+" "+"Folder has not been deleted.");
+          }
       }
   }
+  
 }
