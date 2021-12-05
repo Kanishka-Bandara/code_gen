ea65ed79ef4f13b58252064d87c46f8bb1fdc4ed
kanishka_bandara <wmkubandara@gmail.com>
2021-12-05 14:29:58 +0530

    UPDATE::Add JS Update field generator

--- src/main/java/com/aradnab/code_gen/controllers/JSCOdeGenerateController.java
+++ src/main/java/com/aradnab/code_gen/controllers/JSCOdeGenerateController.java
@@ -4,10 +4,34 @@
  */
 package com.aradnab.code_gen.controllers;
 
+import com.aradnab.code_gen.models.Column;
+
 /**
  *
  * @author kanishka
  */
 public class JSCOdeGenerateController {
-    
+
+    public static JSCOdeGenerateController controller = new JSCOdeGenerateController();
+
+    public String generateJSVariableForSetters(Column c) {
+        String js = "$('#" + c.getColumnJsLabelId() + "').val(data." + c.getSqlName() + ");";
+        switch (c.getColumnHtmlFieldType()) {
+            case "img":
+                js = "                let " + c.getColumnJsName() + " = imageFolderPath+data." + c.getSqlName() + ";\n"
+                        + "                if (" + c.getColumnJsName() + "!=null){\n"
+                        + "                    $(\"#" + c.getColumnJsImageSrcPreviewId() + "\").attr(\"src\", " + c.getSqlName() + ");\n"
+                        + "                }";
+                break;
+            case "cb":
+                break;
+            case "rb":
+                js = "                $('#" + c.getColumnJsLabelId() + "' + data." + c.getSqlName() + ").attr('checked', true);";
+                break;
+            default:
+                js = "$('#" + c.getColumnJsLabelId() + "').val(data." + c.getSqlName() + ");";
+                break;
+        }
+        return js;
+    }
 }
